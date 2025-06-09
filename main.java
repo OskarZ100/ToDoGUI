96f964d1de6165986acce4d3da285316333d0a2e
oskar <oskaraszinecnko500@gmail.com>
2025-06-08 21:04:03 -0400

    Basic set up of frame

--- main.java
+++ main.java
@@ -1,33 +1,35 @@
 // Java program using label (swing)
 // to display the message “GFG WEB Site Click”
 import java.io.*;
+import java.awt.Color;
 import javax.swing.*;
 
 // Main class
 class GFG {
-
+    public static Color c_main = new Color(46, 46, 45);
     // Main driver method
     public static void main(String[] args)
     {
         // Creating instance of JFrame
         JFrame frame = new JFrame();
+        frame = initialSetUp(frame);
+        
 
-        // Creating instance of JButton
-        JButton button = new JButton("TEST");
+    }
 
-        // x axis, y axis, width, height
-        button.setBounds(150, 200, 220, 50);
-
-        // adding button in JFrame
-        frame.add(button);
-
-        // 400 width and 500 height
-        frame.setSize(500, 600);
-
-        // using no layout managers
+    public static JFrame initialSetUp(JFrame frame){
+        //setting basic contraints
+        frame.setTitle("!TO DO APP!");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setSize(700, 700);
         frame.setLayout(null);
-
-        // making the frame visible
         frame.setVisible(true);
+
+        //making the logo icon thing
+        ImageIcon logo = new ImageIcon("logo.png");
+        frame.setIconImage(logo.getImage());
+        frame.getContentPane().setBackground(c_main);
+        return frame;
     }
 }
